0d5139082ef33cc6c0b680c3aa0c00019b233cbd
ruudz <ruudzonnenberg@gmail.com>
2019-01-29 10:22:59 +0000

    Updated LoginController, User table and routes.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -18,22 +20,86 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
-
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider($platform)
+    {
+        return Socialite::driver($platform)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @param $platform
+     * @return void
+     */
+    public function handleProviderCallback($platform)
+    {
+        $userData = Socialite::driver($platform)->user();
+
+        $this->getExistingUser($userData, $platform);
+    }
+
+
+    /**
+     * Try and get existing user and return it or create new one.
+     *
+     * @param $userData
+     * @param string $platform
+     * @return \App\User
+     */
+    public function getExistingUser($userData, $platform)
+    {
+        $user = User::where('social_id', $userData->id)->first();
+
+        if (!$user) {
+            $user = $this->registerNewUser($userData, $platform);
+        }
+
+        return $user;
+    }
+
+    /**
+     * Register a new user with the data given and return
+     *
+     * @param $userData
+     * @param string $platform
+     * @return \App\User
+     */
+    public function registerNewUser($userData, $platform)
+    {
+        // create user
+        $user = User::create([
+            'name' => $userData->name,
+            'username' => str_slug($userData->name) . rand(1, 9999999),
+            'email' => $userData->email,
+            'avatar' => $userData->avatar,
+            'password' => bcrypt(str_random(30)),
+            'social_id' => $userData->id,
+            'platform' => $platform,
+            'token' => $userData->token,
+            'refresh_token' => $userData->refreshToken
+        ]);
+
+        return $user;
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,12 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('social_id');
+            $table->string('platform');
+            $table->text('token');
+            $table->text('refresh_token')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
