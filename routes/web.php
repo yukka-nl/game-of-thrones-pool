403d525f709eef9b81c64edb4c65e490a8b642be
Thijs Kuilman <thijs@yukka.nl>
2019-01-29 13:44:00 +0100

    Fixed merge conflict

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,13 @@
 
 Route::get('/', 'HomeController');
 
-<<<<<<< HEAD
-
 Route::get('/prediction', 'PredictionController@show');
 Route::post('/prediction', 'PredictionController@store');
-
 
 Route::post('/group', 'GroupController@store');
 Route::get('/group/{name}', 'GroupController@show');
 Route::get('/group/invite/{inviteCode}', 'GroupController@join');
 
-
-=======
->>>>>>> master
 // Social Logins
 Route::get('login/{platform}', 'Auth\LoginController@redirectToProvider');
 Route::get('login/{platform}/callback', 'Auth\LoginController@handleProviderCallback');
