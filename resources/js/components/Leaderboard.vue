43bef7ffc046eeecdc6598eeae26f0cdde083bf1
Thijs Kuilman <thijs@yukka.nl>
2019-01-30 13:17:02 +0100

    Improved styling of leaderboard

--- resources/js/components/Leaderboard.vue
+++ resources/js/components/Leaderboard.vue
@@ -9,12 +9,13 @@
             </b-input-group>
         </b-form-group>
 
-        <b-table striped hover :items="items" :fields="fields" v-if="items" :perPage="25" :filter="filter" @filtered="onFiltered">
+        <b-table striped hover :items="items" :fields="fields" v-if="items" :perPage="25" :filter="filter"
+                 @filtered="onFiltered" class="leaderboard-table">
             <template slot="Place" slot-scope="data">
                 {{ data.index + 1 }}
             </template>
             <template slot="avatar" slot-scope="data">
-                <img :src="data.item.avatar" class="rounded-circle mr-2" style="height: 50px;">
+                <img :src="data.item.avatar" class="rounded-circle mr-2" style="height: 25px;">
             </template>
         </b-table>
         <b-pagination size="md" :total-rows="items.length" v-model="currentPage" :per-page="25">
@@ -61,7 +62,7 @@
         mounted() {
         },
         methods: {
-            onFiltered (filteredItems) {
+            onFiltered(filteredItems) {
                 // Trigger pagination to update the number of buttons/pages due to filtering
                 this.totalRows = filteredItems.length;
                 this.currentPage = 1;
