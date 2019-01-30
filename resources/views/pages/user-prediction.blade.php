a832140b4756ee235027f379ec676be041f3c0f6
Thijs Kuilman <thijs@yukka.nl>
2019-01-30 14:48:45 +0100

    Added my prediction page

---
+++ resources/views/pages/user-prediction.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.master')
+
+@section('content')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Predictions of {{ $user->name }}</li>
+        </ol>
+    </nav>
+
+    <div class="container card  mt-3 mb-3 card p-4">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card bg-light mb-4">
+                    <div class="card-body text-center">
+                        <div class="mb-4">
+                            <img src="{{ $user->avatar }}" class="rounded-circle mr-2" style="height: 75px">
+                        </div>
+                        <h1 class="h4">Predictions of {{ $user->name }}</h1>
+                        <span class="h5 text-muted">Correct guesses: {{ $user->correct_guesses }}</span>
+                    </div>
+                </div>
+
+                @if(!$user->hasPredictions())
+                    @if(Auth::check())
+                        @if(Auth::id() === $user->id)
+                            You haven't made any predictions yet.
+                            <a href="/prediction/create">
+                                Click here to make a prediction.
+                            </a>
+                        @endif
+                    @else
+                        This user did not make a prediction yet.
+                    @endif
+                @endif
+
+                @if($user->hasPredictions())
+
+                    @isset($groupedPredictions[1])
+                        <div class="card bg-green mb-3">
+                            <div class="card-body text-white pl-3 pr-3 pt-3 pb-3">
+                                <h1 class="h4 m-0"><i class="fas fa-heartbeat mr-1"></i> Stays alive</h1>
+                            </div>
+                        </div>
+                        @include('partials.character-sheet', ['predictions' => $groupedPredictions[1]])
+                    @endisset
+
+                    @isset($groupedPredictions[2])
+                        <div class="card bg-red mb-3 mt-4">
+                            <div class="card-body text-white pl-3 pr-3 pt-3 pb-3">
+                                <h1 class="h4 m-0"><i class="fas fa-skull-crossbones mr-1"></i> Dies</h1>
+                            </div>
+                        </div>
+                        @include('partials.character-sheet', ['predictions' => $groupedPredictions[2]])
+                    @endisset
+
+                    @isset($groupedPredictions[3])
+                        <div class="card bg-blue mb-3 mt-4">
+                            <div class="card-body text-white pl-3 pr-3 pt-3 pb-3">
+                                <h1 class="h4 m-0"><i class="fas fa-skull mr-1"></i> Becomes a wight</h1>
+                            </div>
+                        </div>
+                        @include('partials.character-sheet', ['predictions' => $groupedPredictions[3]])
+                    @endisset
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
