df44936161d6d0e5e8921929bc00c386ebea337e
Thijs Kuilman <thijs@yukka.nl>
2019-01-30 21:51:18 +0100

    Added og:image

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,6 +23,8 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
+    <meta property="og:image" content="{{ URL::asset('img/site-preview.png') }}" />
+
     <title>Pool of Dragons</title>
 
     <!-- Fonts -->
