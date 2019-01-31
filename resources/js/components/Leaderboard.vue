931285afbe40f6b0a9e60e35310d3d1ebb288376
Thijs Kuilman <thijs@yukka.nl>
2019-01-31 15:34:25 +0100

    Added default fallback for avatars in leaderboards

--- resources/js/components/Leaderboard.vue
+++ resources/js/components/Leaderboard.vue
@@ -12,7 +12,10 @@
         <b-table striped hover :items="items" :fields="fields" v-if="items" :per-page="pageSize" :filter="filter"
                  @filtered="onFiltered" class="leaderboard-table" :current-page="currentPage" responsive>
             <template slot="avatar" slot-scope="data">
-                <img :src="data.item.avatar" class="rounded-circle mr-2" style="height: 25px;">
+                <img :src="data.item.avatar" v-if="data.item.avatar" class="rounded-circle mr-2"
+                     style="height: 25px; width: 25px;">
+                <img src="/img/default-avatar.png" v-if="!data.item.avatar" class="rounded-circle mr-2"
+                     style="height: 25px; width: 25px;">
             </template>
             <template slot="correct_guesses" slot-scope="data">
                 <span>{{ data.item.correct_guesses }}</span>
@@ -26,7 +29,8 @@
             </template>
         </b-table>
 
-        <b-pagination size="md" :total-rows="items.length" v-model="currentPage" :per-page="pageSize" align="center" class="mt-3"></b-pagination>
+        <b-pagination size="md" :total-rows="items.length" v-model="currentPage" :per-page="pageSize" align="center"
+                      class="mt-3"></b-pagination>
     </div>
 </template>
 
@@ -39,7 +43,7 @@
         },
         data() {
             return {
-                pageSize : 25,
+                pageSize: 25,
                 fields: [
                     {
                         key: 'ranking',
