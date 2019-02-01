48bd7185fa59387b251c733e46696455d26c8dc1
Thijs Kuilman <thijs@yukka.nl>
2019-02-01 15:28:58 +0100

    Temporary disabled Twitter login until it is fixed on production

--- resources/views/partials/social-login-buttons.blade.php
+++ resources/views/partials/social-login-buttons.blade.php
@@ -7,9 +7,9 @@
         <i class="fab fa-reddit brand-color-reddit mr-1"></i> Reddit
     </a>
 
-    <a href="/login/twitter" class="btn btn-outline-secondary mt-1 mr-2">
-        <span class="fab fa-twitter brand-color-twitter mr-1"></span> Twitter
-    </a>
+    {{--<a href="/login/twitter" class="btn btn-outline-secondary mt-1 mr-2">--}}
+        {{--<span class="fab fa-twitter brand-color-twitter mr-1"></span> Twitter--}}
+    {{--</a>--}}
 
     <a href="/login/google" class="btn btn-outline-secondary mt-1">
         <i class="fab fa-google brand-color-google mr-1"></i> Google
